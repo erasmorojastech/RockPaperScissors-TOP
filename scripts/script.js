ddba62d1d73e4206c39ed05c421d726c6a6d07b6
erasmorojastech <erasmorojastech@gmail.com>
2023-02-06 10:51:06 -0300

    Add a "playRound" function
    This function plays a single round of the game and returns the result
    as a text.

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,18 @@ function getComputerChoice() {
     return 'Scissors';
   }
 };
+
+// Playing a single round of the game
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toLowerCase(),
+    computer = computerSelection.toLowerCase();
+
+  if (player === computer) return 'Tie';
+  if (player === 'rock' && computer === 'paper') return 'You lose! Paper covers Rock';
+  if (player === 'rock' && computer === 'scissors') return 'You win! Rock smashes Scissors';
+  if (player === 'paper' && computer === 'rock') return 'You win! Paper covers Rock';
+  if (player === 'paper' && computer === 'scissors') return 'You lose! Scissors cuts paper';
+  if (player === 'scissors' && computer === 'rock') return 'You lose! Rock smashes Scissors';
+  if (player === 'scissors' && computer === 'paper') return 'You win! Scissors cuts Paper';
+
+}
