95447b66fc14d0a2b719f1a8fbada830121cd1af
erasmorojastech <erasmorojastech@gmail.com>
2023-12-06 12:10:00 -0300

    Add "getComputerChoice" function to the script
    It randomly returns one of the following string values: "rock",
    "paper" or "scissors"

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let random_choice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    let choice = ''
+
+    if(random_choice === 1)
+        choice = 'rock'
+    else if (random_choice === 2)
+        choice = 'paper'
+    else
+        choice = 'scissors'
+
+    return choice
+}
+
+console.log(getComputerChoice());
