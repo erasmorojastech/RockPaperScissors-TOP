754ddd488d215af2baf68075b1b046ce8b2e571f
erasmorojastech <erasmorojastech@gmail.com>
2026-02-20 12:00:00 -0300

    feat: Implement logic for random computer selection

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,25 @@
 'use strict';
 
-console.log("from console");
+const getComputerChoice = function () {
+  const randomNumberChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+  // Map random numbers to game options (Rock, Paper or Scissors)
+
+  let computerChoice = '';
+
+  switch (randomNumberChoice) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "raper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+    default:
+      break;
+  }
+
+  return computerChoice;
+};
 
